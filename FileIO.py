b3e16a967cf0ceec9f459453dd18dc0c72fb163c
Akanksha <akankshaf7@gmail.com>
2024-12-05 21:07:35 +0530

    First Commit

---
+++ FileIO.py
@@ -0,0 +1,40 @@
+'''
+The random-access memory is volatile, and all its contents are lost once a program terminates in order to persist the data forever, we use files.
+A file is data stored in a storage device. A python program can talk to the file by reading content from it and writing content to it.
+RAM = Volatile
+HDD/SDD/Hrddisk/Pendrive = Non Volatile
+TYPES OF FILES
+There are 2 types of files:
+1)Text Files (.txt, .c etc.)
+2)Binary Files(.jpg, .dat, etc.)
+Python has a lot of functions for reading, updating, and deleting files.
+
+OPENING A FILE
+Python has an open() function for opening files. It takes 2 parameters: filename and made
+'''
+#READING A FILE IN PYTHON
+#open the file in read mode
+f = open("file.txt","r")
+#Read its contents
+data = f.read()
+#Print its contents
+print(data)
+#close the file
+f.close()
+
+#OTHER METHODS TO READ THE FILE
+#We can also use f.readline() function to read one full line at a time.
+#f.readline() = Read one line from the file.
+'''
+MODES OF OPENING A FILE
+
+r = open for reading
+w = open for writing
+a = open for appending
++ = open for updating
+'rb' will open for read in binary mode
+'rt' will open for read in text mode.
+
+'''
+
+f.open("file.txt")
---
+++ file_write.py
@@ -0,0 +1,5 @@
+st="Akanksha is an intermediate level student in the field of Artificial Intelligence and Machnie Learning."
+
+f=open("myfile.txt","w")
+f.write(st)
+f.close()
